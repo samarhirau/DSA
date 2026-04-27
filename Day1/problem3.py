d15aee05620d7715f0aa846a5f0f4cc43a7f56f3
samarhirau <samrhirau@gmail.com>
2026-04-27 13:47:04 +0530

    solved

--- Day1/problem3.py
+++ Day1/problem3.py
@@ -11,7 +11,26 @@ def max_subarray(nums):
     return max_sum
 
 
-arr = [-2,1,-3,4,-1,2,1,-5,4]
-print(max_subarray(arr))
+# arr = [-2,1,-3,4,-1,2,1,-5,4]
+# print(max_subarray(arr))
 
 # Kadane’s Algorithm (Optimal Solution)
+
+def max_subarray(nums):
+    current_sum = nums[0]
+    max_sum = nums[0]
+    start = end = temp_start = 0
+    
+    for i in range(1, len(nums)):
+        if current_sum < 0:
+            current_sum = nums[i]
+            temp_start = i
+        else:
+            current_sum += nums[i]
+        
+        if current_sum > max_sum:
+            max_sum = current_sum
+            start = temp_start
+            end = i
+    
+    return nums[start:end+1]
